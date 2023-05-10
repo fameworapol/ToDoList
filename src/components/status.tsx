399b8b871d3dbb0e3ef79bd75932646c446d8e87
Worapol Thongmug <worapolthongmug@MacBook-Pro-khxng-Worapol.local>
2023-05-10 19:46:27 +0700

    add delete feature

--- src/components/status.tsx
+++ src/components/status.tsx
@@ -1,6 +1,7 @@
 import { useState ,useContext,useEffect} from "react";
 import Item from "../model/items";
 import DataContext from "../context/dataContext";
+import '../style/status.css'
 
 interface Statusprops{
     item:Item
@@ -10,15 +11,12 @@ export default function Status(props:Statusprops) {
     const data = useContext(DataContext)
     const [isChecked, setisChecked] = useState(props.item.complete)
     async function handlerCheckbox(e: React.ChangeEvent<HTMLInputElement>) {
-        console.log("before :",isChecked);
         if (isChecked == false) {
             setisChecked(true)
-            //console.log(isChecked); 
         }else{
             setisChecked(false)
-            //console.log("change");
         }
-        //console.log("after :",isChecked);
+        
         window.location.reload()
     }
     useEffect(() => {
@@ -35,15 +33,24 @@ export default function Status(props:Statusprops) {
             }
             return
         })
-        console.log(props.item.complete)
     }
     function upDateData(){
         localStorage.setItem("myList",JSON.stringify(data))
     }
+    function deleteTask(){
+        for (let i = 0; i < data.length; i++) {
+            if(data[i].id==props.item.id){
+                data.splice(i,1)
+                localStorage.setItem("myList",JSON.stringify(data))
+                window.location.reload()
+            }
+        }
+    }
     return (
-        <div className={`form-check form-switch`}>
+        <div className={`status form-check form-switch`}>
             <input className={`form-check-input ${props.item.complete ? "bg-success":""}`} type="checkbox" 
             role="switch" id="flexSwitchCheckDefault" checked={props.item.complete} onChange={handlerCheckbox} />
+            <button className="deleteTask" onClick={deleteTask}>☒</button>
         </div>
     )
 }
